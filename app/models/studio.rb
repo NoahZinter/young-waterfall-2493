3f4049b97801599250d8df4629514c74c0fe92b9
NoahZinter <noah.zinter@gmail.com>
2021-06-01 10:06:55 -0600

    implement studio relationships

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,5 @@
 class Studio < ApplicationRecord
   has_many :movies
+  has_many :actor_movies, through: :movies
+  has_many :actors, through: :actor_movies
 end
--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Studio do
   describe 'relationships' do
     it {should have_many :movies}
+    it {should have_many(:actor_movies).through(:movies)}
+    it {should have_many(:actors).through(:actor_movies)}
   end
 end
