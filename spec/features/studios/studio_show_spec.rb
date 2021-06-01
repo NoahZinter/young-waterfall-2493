42edbf4cefee26fb296b190cc2eac7e21349ab2a
NoahZinter <noah.zinter@gmail.com>
2021-06-01 09:43:28 -0600

    Test list of movie titles

--- spec/features/studios/studio_show_spec.rb
+++ spec/features/studios/studio_show_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'studio show' do
   before :each do
     @marvel = Studio.create!(name: 'Marvel', location: 'Hollywood')
+    @avengers = @marvel.movies.create!(title:'Avengers', creation_year: 1990, genre:'Action')
+    @wolverine = @marvel.movies.create!(title:'Wolverine', creation_year: 1998, genre:'Drama')
     visit "/studios/#{@marvel.id}"
   end
   
@@ -10,5 +12,10 @@ describe 'studio show' do
     expect(page).to have_content("Marvel details:")
     expect(page).to have_content("Studio Name: Marvel")
     expect(page).to have_content("Studio Location: Hollywood")
+  end
+
+  it 'displays names of all movies for studio' do
+    expect(page).to have_content('Avengers')
+    expect(page).to have_content('Wolverine')
   end
 end
