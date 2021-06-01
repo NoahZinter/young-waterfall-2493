9b4cb5d92144826d3d650c79f85c61bb42ec58a0
NoahZinter <noah.zinter@gmail.com>
2021-06-01 10:56:37 -0600

    Test show

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,5 +2,6 @@ class StudiosController < ApplicationController
   def show
     @studio = Studio.find(params[:id])
     @movies = @studio.movies
+    @actors = @studio.working_actors
   end
 end
--- spec/features/studios/studio_show_spec.rb
+++ spec/features/studios/studio_show_spec.rb
@@ -5,6 +5,13 @@ describe 'studio show' do
     @marvel = Studio.create!(name: 'Marvel', location: 'Hollywood')
     @avengers = @marvel.movies.create!(title:'Avengers', creation_year: 1990, genre:'Action')
     @wolverine = @marvel.movies.create!(title:'Wolverine', creation_year: 1998, genre:'Drama')
+    @ford = Actor.create!(name: 'Harrison Ford', age: 40, working: true, movie_id: @avengers.id)
+    @ferrell = Actor.create!(name: 'Will Ferrell', age: 50, working: true, movie_id: @wolverine.id )
+    @johanssen = Actor.create!(name: 'Scarlett Johanssen', age: 30, working: false, movie_id: @avengers.id)
+    @join_1 = ActorMovie.create!(actor_id: @ford.id, movie_id: @avengers.id)
+    @join_1 = ActorMovie.create!(actor_id: @ford.id, movie_id: @wolverine.id)
+    @join_2 = ActorMovie.create!(actor_id: @ferrell.id, movie_id: @wolverine.id)
+    @join_3 = ActorMovie.create!(actor_id: @johanssen.id, movie_id: @avengers.id)
     visit "/studios/#{@marvel.id}"
   end
   
@@ -17,5 +24,10 @@ describe 'studio show' do
   it 'displays names of all movies for studio' do
     expect(page).to have_content('Avengers')
     expect(page).to have_content('Wolverine')
+  end
+
+  it 'displays all working actors for a studio' do
+    expect(page).to have_content('Will Ferrell')
+    expect(page).to have_content('Harrison Ford')
   end
 end
