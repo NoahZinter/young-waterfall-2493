dd3e6a535ff47fe07a1fe112f3374d521dca29c1
NoahZinter <noah.zinter@gmail.com>
2021-06-01 10:03:54 -0600

    Test relationships

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Actor, type: :model do
   describe 'relationships' do
     it { should belong_to :movie}
+    it {should have_many :actor_movies}
+    it {should have_many(:movies).through(:actor_movies)}
   end
 end
